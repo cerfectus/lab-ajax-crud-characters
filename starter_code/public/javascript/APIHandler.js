46fd60beb4020336d91e8f09343c918a959f973b
lucas <cerfectus@gmail.com>
2018-10-17 14:04:17 -0500

    dun

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,82 @@
+const allData = (name, occupation, weapon, cartoon) => {
+  $(".characters-container").append(`<div class="character-info">
+  <div class="name">Character Name</div>
+  <div class="occupation">Character Occupation</div>
+  <div class="cartoon">Is a Cartoon?</div>
+  <div class="weapon">Character Weapon</div>`)
+}
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    axios.get(`${this.BASE_URL}/characters`)
+    .then(res => {
+        console.log(res.data)
+        res.data.forEach(e => {
+          allData(e.name, e.occupation, e.weapon, e.cartoon)
+        })
+    })
+    
   }
 
-  getOneRegister () {
-
+  getOneRegister (charID) {
+    axios.get(`${this.BASE_URL}/characters/${charID}`)
+    .then(res => {
+        console.log(res.data)
+        res.data.forEach(e => {
+          allData(e.name, e.occupation, e.weapon, e.cartoon)
+        })
+    })
   }
 
-  createOneRegister () {
+  createOneRegister (name, occupation, weapon, cartoon) {
+    const newChar = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon,
+    }
+      axios.post(`${this.BASE_URL}/characters`,newChar)
+      .then( res => {
 
+      })
+        this.getFullList();
+
+      }
+    
   }
 
   updateOneRegister () {
+    const updateChar = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon,
+    }
+      axios.post(`${this.BASE_URL}/characters`,updateChar)
+      .then( res => {
 
-  }
+      })
+        this.getFullList();
+
+      };
+
 
   deleteOneRegister () {
+    const newChar = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon,
+    }
+      axios.post(`${this.BASE_URL}/characters`,newChar)
+      .then( res => {
 
-  }
-}
+      })
+        this.getFullList();
+
+      }
+  
+
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,17 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    $(".character-info").detach();
+    charactersAPI.getFullList()
+
 
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let charID = document.getElementById("fetch.one-value").nodeValue
+    $(".character-info").detach();
+    charactersAPI.getOneRegister();
+
   }
   
   document.getElementById('delete-one').onclick = function(){
